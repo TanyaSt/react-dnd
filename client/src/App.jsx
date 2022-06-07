294952cde1d94132040edb4a2de518923e8084da
Tanya <tanstmail.nik@gmail.com>
2022-06-07 15:49:02 +0300

    Initial commit

---
+++ client/src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Homepage from "./pages/Homepage";
+import Header from "./components/Header";
+import { DndProvider } from "react-dnd";
+import Backend from "react-dnd-html5-backend"
+
+const App = () => {
+    return (
+        <DndProvider backend={Backend}>
+            <Header />
+            <Homepage />
+        </DndProvider>
+
+    );
+};
+
+export default App;
